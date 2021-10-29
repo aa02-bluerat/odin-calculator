4a2933bc15eeb8502918412a5fc4f770ad28d688
aa02-bluerat <blueratdota@gmail.com>
2021-10-29 16:23:00 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # odin-calculator
-wip
+# v1.0
+# -all functions work
+# ::::will work on limiting decimal answers
+# ::::will add more css(make responsive)
+
+
+# fonts-->Google Fonts:Roboto Mono
---
+++ script.js
@@ -0,0 +1,115 @@
+//screen text
+const topScreen = document.querySelector('.topS');
+const botScreen = document.querySelector('.botS');
+
+//all buttons
+const buttonNumber = document.querySelectorAll('.btnNumber');
+const buttonOperator = document.querySelectorAll('.operation');
+const buttonDel = document.querySelector('#butn-del');
+const buttonAc = document.querySelector('#butn-ac');
+
+//START--TEMP CONTENT
+let botScTempContent =[];
+let topScTempContent =[];
+
+//FOR OPERATION 
+let newValue =0;
+
+//START--ALL FUNCTIONS
+
+function combineInput(){
+    const combinedNum = botScTempContent.join('');
+    console.log(combinedNum);
+    botScreen.textContent = combinedNum;
+}
+
+function updateScreen(){
+    topScreen.textContent = botScreen.textContent;
+    botScreen.textContent=newValue;
+}
+
+//END--ALL FUNCTIONS
+
+buttonDel.addEventListener('click', function(){
+    botScTempContent.pop();
+    combineInput();
+})
+
+buttonAc.addEventListener('click', function(){
+    botScTempContent =[];
+    botScreen.textContent = '';
+    topScreen.textContent = '';
+})
+
+for(let i=0; i<buttonNumber.length; i++){
+    buttonNumber[i].addEventListener('click', function(){
+        console.log(buttonNumber[i].textContent);
+        botScTempContent.push(buttonNumber[i].textContent);
+        combineInput(); 
+    })
+}
+
+
+for(let i=0; i<buttonOperator.length; i++){
+    buttonOperator[i].addEventListener('click', function(){
+        console.log(botScTempContent);
+
+        if(buttonOperator[i].textContent === '+'){
+            newValue = eval(botScTempContent.join(''));
+            botScTempContent=[newValue];
+            botScTempContent.push('+');
+            updateScreen();           
+        }
+
+        else if(buttonOperator[i].textContent === '-'){
+            newValue = eval(botScTempContent.join(''));
+            botScTempContent=[newValue];
+            botScTempContent.push('-');
+            console.log(newValue);
+            updateScreen();
+        }
+
+        else if(buttonOperator[i].textContent === 'x'){
+            newValue = eval(botScTempContent.join(''));
+            botScTempContent=[newValue];
+            botScTempContent.push('*');
+            console.log(newValue);
+            updateScreen();
+        }
+
+        else if(buttonOperator[i].textContent === '÷'){
+            newValue = eval(botScTempContent.join(''));
+            botScTempContent=[newValue];
+            botScTempContent.push('/');
+            console.log(newValue);
+            updateScreen();
+        }
+
+        else if(buttonOperator[i].textContent === 'sqrt'){
+            newValue = eval(botScTempContent.join('')**0.5);
+            botScTempContent=[newValue];
+            console.log(newValue);
+            topScreen.textContent = botScreen.textContent+'√';
+            botScreen.textContent=newValue;
+        }
+
+        else if(buttonOperator[i].textContent === '='){
+            newValue = eval(botScTempContent.join(''));
+            botScTempContent=[newValue];
+            console.log(newValue);
+            updateScreen();
+        }
+
+        
+    })
+}
+
+
+
+
+
+
+
+
+
+    
